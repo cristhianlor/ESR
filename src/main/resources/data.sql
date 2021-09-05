81490243171061d0cb960fa954f0dc40fa2a5c36
Cristiano Assis <cristiano.aa@gmail.com>
2021-09-05 20:45:10 -0300

    Ajustes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ insert into tb_cozinha (cozinha) values (null, 'Chilena');
 insert into tb_cozinha (cozinha) values (null,'Argentina');
 insert into tb_cozinha (cozinha) values (null, 'Uruguaia');
 
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Brasil', 10.5, 1);
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Brasil 1', 12.5, 1);
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Chile', 9.5, 2);
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Chile 2', 8.5, 2);
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Argentina', 7.5, 3);
-insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values ('Uruaguaia', 6.5, 4);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Brasil', 10.5, 1);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Brasil 1', 12.5, 1);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Chile', 9.5, 2);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Chile 2', 8.5, 2);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Argentina', 7.5, 3);
+insert into tb_restaurante (nome_restaurante, taxa_frete, cozinha_id) values (null,'Uruaguaia', 6.5, 4);
